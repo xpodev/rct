1e9e98005c25877638080ab0e1bfb27363481ff8
Neriya Cohen <ee10293847@gmail.com>
2022-03-16 21:27:57 +0200

    Update scripts

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,5 @@
 @echo off
 
-cd ..
 call npm install
 call npm run build
 xcopy /e /y src\generate\templates bin\generate\templates\
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cd ..
-
 npm install --silent
 npm run build
 cp -r src/generate/templates bin/generate/templates
